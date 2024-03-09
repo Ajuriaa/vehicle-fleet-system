46e2f8ef7ba34f27e92f14021ca93f8fb852ec0d
Alejandro Ajuria <alejoajuria@gmail.com>
2024-03-08 21:41:58 -0600

    login component modified and route added

--- src/app/auth/containers/login/login.component.ts
+++ src/app/auth/containers/login/login.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {ErrorStateMatcher} from '@angular/material/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  public loginForm: FormGroup;
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
 
+  matcher = new ErrorStateMatcher();
+
+  constructor(private _formBuilder: FormBuilder) {
+    this.loginForm = this._formBuilder.group({
+      username: [''],
+      password: ['']
+    });
+  }
+
+  ngOnInit(): void {
+    this.loginForm = this._formBuilder.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
 }
