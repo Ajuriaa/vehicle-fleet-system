49887c603731f69d479c8c05bf902d0fbf81cb5a
Alejandro Ajuria <alejoajuria@gmail.com>
2024-04-23 16:15:09 -0600

    using hash location strategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,12 +8,14 @@ import { MAT_DATE_LOCALE } from '@angular/material/core';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { routes } from './app.routes';
 import { cookieHelper } from './core/helpers';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), provideAnimationsAsync('noop'),
     provideAnimations(), provideToastr(), provideHttpClient(withFetch()), provideAnimationsAsync(),
     { provide: MAT_DATE_LOCALE, useValue: 'es-ES' },
+    { provide: LocationStrategy, useClass: HashLocationStrategy},
     cookieHelper,
     provideCharts(withDefaultRegisterables())
   ]
